481758d9abfa30a8184631405d15ac5bd4f13b4f
miyauchikazuyoshi <miyauchikazuyoshi@gmail.com>
2025-05-20 22:00:53 +0900

    Ci_high_layer_fix

--- tests/unit/test_agent_loop.py
+++ tests/unit/test_agent_loop.py
@@ -6,7 +6,7 @@ sys.modules['rich'] = types.SimpleNamespace(print=lambda *a, **k: None)
 
 embed_mod = types.SimpleNamespace(get_model=lambda: types.SimpleNamespace(encode=lambda x, normalize_embeddings=True: [[0.0]]))
 layer1 = types.SimpleNamespace(uncertainty=lambda x: 0.0)
-layer2 = types.SimpleNamespace(Memory=types.SimpleNamespace(search=lambda self, q, k: [(1.0,0)], update_c=lambda self, idxs, r, eta=0.1: None, train_index=lambda self: None, merge=lambda self, idxs: None, split=lambda self, idx: None, prune=lambda self,c,i: None, add_episode=lambda self,v,t,c_init=0.2: None, save=lambda self: importlib.import_module('pathlib').Path('p'))))
+layer2 = types.SimpleNamespace(Memory=types.SimpleNamespace(search=lambda self, q, k: [(1.0,0)], update_c=lambda self, idxs, r, eta=0.1: None, train_index=lambda self: None, merge=lambda self, idxs: None, split=lambda self, idx: None, prune=lambda self,c,i: None, add_episode=lambda self,v,t,c_init=0.2: None, save=lambda self: importlib.import_module('pathlib').Path('p')))
 layer3 = types.SimpleNamespace(build_graph=lambda vecs: (types.SimpleNamespace(num_nodes=0, edge_index=types.SimpleNamespace(numpy=lambda: [[0],[0]])), None))
 metrics = types.SimpleNamespace(delta_ged=lambda g1, g2: 0.0, delta_ig=lambda v1,v2:0.0)
 layer4 = types.SimpleNamespace(generate=lambda prompt: 'ans')
